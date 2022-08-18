5024bc03010296c3eb9742d77218536e2349cebd
Ben Mechen <ben@chelsea-apps.com>
2022-08-18 11:20:16 +0100

    Fix message interface import

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,3 @@
 export * from './customerio-options.interface';
 export * from './customerio-module-async-options.interface';
 export * from './customerio-options-factory.interface';
-export * from './message.interface';
